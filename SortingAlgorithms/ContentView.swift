b5651f15303c3b540942914ec2193a8de55b1144
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-06-01 10:26:24 +0200

    Initial Commit

---
+++ SortingAlgorithms/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SortingAlgorithms
+//
+//  Created by Tommy Ludwig on 01.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingAlgorithmsApp.swift
+//  SortingAlgorithms
+//
+//  Created by Tommy Ludwig on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SortingAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
